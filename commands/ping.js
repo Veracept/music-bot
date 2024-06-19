4a84f3aceeb5813411d6f324bca4e9766a69e462
Veracept <106467557+Veracept@users.noreply.github.com>
2024-06-19 15:16:40 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -22,7 +22,7 @@ const { EmbedBuilder } = require('discord.js')
 const db = require("../mongoDB");
 module.exports = {
   name: "ping",
-  description: "check the bot latency",
+  description: "Проверьте задержку бота",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
