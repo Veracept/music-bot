77e0170d4fb62a0bc12169f0c7fdccb85266f482
Veracept <106467557+Veracept@users.noreply.github.com>
2024-06-19 14:48:53 +0200

    Update filter.js

--- commands/filter.js
+++ commands/filter.js
@@ -1,7 +1,7 @@
 const db = require("../mongoDB");
 module.exports = {
   name: "filter",
-  description: "Adds audio filter to ongoing music.",
+  description: "Добавляет аудиофильтр к текущей музыке.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -9,7 +9,7 @@ module.exports = {
     try {
       const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
       const queue = client?.player?.getQueue(interaction?.guild?.id);
-      if (!queue || !queue?.playing) return interaction?.reply({ content: '⚠️ No music playing!!', ephemeral: true }).catch(e => { })
+      if (!queue || !queue?.playing) return interaction?.reply({ content: '⚠️ Музыка не играет!!', ephemeral: true }).catch(e => { })
 
       let buttons = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
@@ -57,7 +57,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157534645311766558/2353-arrowrightglow.gif?ex=6518f5a5&is=6517a425&hm=ce55696f7ed85e2f7a97a3505eb39016fa9cd0c50be043efdf0cce06d7126b4c&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-      .setDescription('** Explore the Beat, Choose Your Sound Magic Below!**')
+      .setDescription('** Исследуйте ритм, выберите свою звуковую магию ниже!**')
   
     interaction.reply({ embeds: [embed], components: [buttons, buttons2] }).then(async Message => {
 
@@ -71,7 +71,7 @@ module.exports = {
 if(!filters?.includes(button?.customId)) return
 
       let filtre = button.customId
-      if (!filtre) return interaction?.editReply({ content: '❌ Invalid Name', ephemeral: true }).catch(e => { })
+      if (!filtre) return interaction?.editReply({ content: '❌ Неверное название', ephemeral: true }).catch(e => { })
      filtre = filtre?.toLowerCase()
    
       if (filters?.includes(filtre?.toLowerCase())) {
@@ -86,7 +86,7 @@ if(!filters?.includes(button?.customId)) return
         }
       } else {
         const filter = filters?.find((x) => x?.toLowerCase() === filtre?.toLowerCase())
-        embed?.setDescription(`❌ Couldn't find filter!!`.replace("{filters}", filters?.map(mr => `\`${mr}\``).join(", ")))
+        embed?.setDescription(`❌ Такого фильтра нету!!`.replace("{filters}", filters?.map(mr => `\`${mr}\``).join(", ")))
         if (!filter) return interaction?.editReply({ embeds: [embed] }).catch(e => { })
       }
     })
