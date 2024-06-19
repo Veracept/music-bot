7f029409f1063e67b065dd7e78a0c0f6773c327b
Veracept <106467557+Veracept@users.noreply.github.com>
2024-06-19 14:55:35 +0200

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -1,29 +1,10 @@
-/*
 
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
 const db = require("../mongoDB");
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "pause",
-  description: "Stops playing the currently playing music.",
+  description: "Останавливает воспроизведение текущей музыки.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -44,7 +25,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157296313013117049/8061-purple-pause-icon.png?ex=651817ae&is=6516c62e&hm=4596c9fab9d8b66de8b5215b2750572ced352eed67440a1134550b846b5693b9&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription(success ? '**The music has been Paused for a moment!!**' : '❌ Command Error: Unable to pause song')
+        .setDescription(success ? '**Музыка была приостановлена ​​на мгновение!!**' : '❌ Command Error: Unable to pause song')
         
 
       return interaction.reply({ embeds: [embed] });
