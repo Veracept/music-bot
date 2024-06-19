30fa91c07b5d9546a2dfa7754e8bc00ba3726500
Veracept <106467557+Veracept@users.noreply.github.com>
2024-06-19 14:47:09 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,29 +1,10 @@
-/*
 
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
 const db = require("../mongoDB");
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "clear",
-  description: "Clears the music queue.",
+  description: "Очищает очередь музыки.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -32,11 +13,11 @@ module.exports = {
     
     try {
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Музыка не играет!!', ephemeral: true });
       }
 
       if (!queue.songs[0]) {
-        return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true });
+        return interaction.reply({ content: '❌ Очередь пуста!!', ephemeral: true });
       }
 
       await queue.stop(interaction.guild.id);
@@ -44,11 +25,11 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#3498db')
         .setAuthor({
-          name: 'Cleared List',
+          name: 'Очищенный список',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157314241393598585/4618-no-slides.png?ex=65182861&is=6516d6e1&hm=dac8fed5a18e1574485e833d4c017591c50f59d161e1bde7fed5f6a92543f951&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription('**Queue cleared! Be Ready for a new musical journey.**')
+        .setDescription('**Очередь очищена! Будьте готовы к новому музыкальному путешествию.**')
        
 
       interaction.reply({ embeds: [embed] });
@@ -58,23 +39,4 @@ module.exports = {
   },
 };
 
-/*
 
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
